6a54ec1c6496273172135343d659c30228c9d34a
Shawn Code <jeremydeshawn@rocketmail.com>
2023-01-04 04:51:03 +0000

    Saved progress on ThrowCustomException 2023-01-04 04:51:03 +0000

--- ThrowCustomException/src/main/java/CustomExceptionThrower.java
+++ ThrowCustomException/src/main/java/CustomExceptionThrower.java
@@ -8,8 +8,16 @@ public class CustomExceptionThrower {
      *
      * I recommend looking at the test case for a good example of try/catch block usage, as well.
      */
-    public void throwCustomException(){
-
+    public void throwCustomException(int age){
+        if(age<0){
+            try {
+                throw new CustomException("Age is a negative number");
+            } catch (CustomException e) {
+                System.out.println("please enter a positive number");
+                e.printStackTrace();
+            }
+        } 
+        else{System.out.println("Age makes sense");}
     }
 
 }
