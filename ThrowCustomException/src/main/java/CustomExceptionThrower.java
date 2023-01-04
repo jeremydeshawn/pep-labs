00a5f1786716d59955b9f774343ac756e7698f9d
Shawn Code <jeremydeshawn@rocketmail.com>
2023-01-04 06:21:56 +0000

    Saved progress on ThrowCustomException 2023-01-04 06:21:56 +0000

--- ThrowCustomException/src/main/java/CustomExceptionThrower.java
+++ ThrowCustomException/src/main/java/CustomExceptionThrower.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 
 public class CustomExceptionThrower {
 
+    static void checkAge(int age) throws CustomException{
+            if(age < 18){
+               throw new CustomException("you must be 18 or older");
+            } else{
+                System.out.println("Old enough to vote");
+            }
+        } 
+    
     /**
      * This method should throw a CustomException. CustomException is a custom exception that we've written ourselves
      * by extending the Exception class in CustomException.java. In a large scale application, writing custom
@@ -10,17 +18,10 @@ public class CustomExceptionThrower {
      *
      * I recommend looking at the test case for a good example of try/catch block usage, as well.
      */
-    CustomException ce = new CustomException(null);
+    
     Scanner sc = new Scanner(System.in);
-    int age = sc.nextInt();
 
-   static void checkAge(int age){
-        if(age < 18){
-            System.out.println("Too young to vote");
-        } else{
-            System.out.println("Old enough to vote");
-        }
-    }
+   int age = sc.nextInt();
     public void throwCustomException() throws CustomException{
         try {
             checkAge(age);
