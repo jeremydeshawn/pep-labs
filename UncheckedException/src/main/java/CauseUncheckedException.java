89ba929832ad669a779b5f0b10371e3f3b27798d
Shawn Code <jeremydeshawn@rocketmail.com>
2023-01-04 03:37:43 +0000

    Saved progress on UncheckedException 2023-01-04 03:37:43 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,7 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        String[] arr = {"lv226","hellion prime", "namek","planey vegeta"};
+        System.out.println(arr[4]);
     }
 }
